b107fe255678b68e59d52d7d56fcc2c9b74bb994
Stift007 <74728802+Stift007@users.noreply.github.com>
2022-03-10 18:03:01 +0100

    Initial Commit

---
+++ __app.py
@@ -0,0 +1,63 @@
+import sqlite3
+from tkinter import *
+from tkinter import filedialog,messagebox,simpledialog, scrolledtext
+from tkinter.ttk import Notebook, Frame, Treeview
+import os
+from traceback import format_exc
+import sys
+
+from tkinter import *
+   
+# Create object
+
+db_path = None
+
+def load_db():
+    global db_path
+    db_path = filedialog.askopenfilename(filetypes=(("SQL Database","*.db")))
+
+def secure_error(error:Exception):
+    win = Tk()
+
+    # This is the section of code which creates the main window
+    win.geometry('520x230')
+    win.configure(background='#FFFFFF')
+    win.title('Internal Error')
+    text_area = scrolledtext.ScrolledText(win, 
+                                      wrap = WORD, 
+                                      width = 65, 
+                                      height = 10, 
+                                      font = ("Arial",
+                                              10))
+    
+    text_area.pack()
+   
+    # Placing cursor in the text area
+    text_area.focus()
+    text_area.insert(INSERT,
+    f"""
+    {format_exc()}
+    """)
+    Button(win,text="Copy to Clipboard",command=lambda:win.clipboard_append(error)).pack()
+    # Making the text read only
+    Button(win,text="Close",command=lambda:win.destroy()).pack()
+    text_area.configure(state ='disabled')
+    
+menu = Menu(root)
+files = Menu(menu)
+files.add_command(label="New Database",command=new_db)
+files.add_command(label="Load Database",command=load_db)
+files.add_command(label="Dump Code",command=dump_db_code)
+
+
+root = Tk()
+root.title("Querify")
+root.geometry("900x600")
+
+try:
+    ff
+except Exception as error:
+    secure_error(error)
+
+mainloop()
+
